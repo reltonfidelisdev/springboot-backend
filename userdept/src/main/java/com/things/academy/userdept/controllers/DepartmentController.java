2233196d24411787e16bf7b2b40c531e5d4071d2
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-20 15:05:41 -0300

    alter mapping methods for http rest api methods get, post, put and delete

--- userdept/src/main/java/com/things/academy/userdept/controllers/DepartmentController.java
+++ userdept/src/main/java/com/things/academy/userdept/controllers/DepartmentController.java
@@ -3,15 +3,16 @@ package com.things.academy.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.things.academy.userdept.entities.Department;
-import com.things.academy.userdept.entities.User;
 import com.things.academy.userdept.repositories.DepartmentRepository;
 
 @RestController
@@ -36,15 +37,22 @@ public class DepartmentController {
 	@PostMapping
 	public Department insert(@RequestBody Department department) {
 		Department result = repo.save(department);
-		return department;
+		return result;
+	}
+	@PutMapping
+	public Department update(@RequestBody Department department) {
+		Department result = repo.saveAndFlush(department);
+		return result;
 	}
 	
-	@PostMapping("/remove")
+	@DeleteMapping
 	public void remove(@RequestBody Department department) {
-		try {
+		try { // try to remove a department. It's possible only if not associated to one or more users
 			repo.delete(department);
 		} catch (Exception e) {
-			System.err.println("Can't remove department. " + e);
+			System.err.println("{'error':'true'}, {'message':'Can\'t remove department. Please verify if it\'s contains an associated foreign key'}");
+			System.err.println("Exception:");
+			System.err.println(e.getMessage() + " : \n" + e.getClass());
 		}
 	}
 
