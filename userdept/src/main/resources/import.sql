4aa6c35baaa3c332442bcde1c04a9114e1e11a0e
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-20 15:19:31 -0300

    Update import.sql

--- userdept/src/main/resources/import.sql
+++ userdept/src/main/resources/import.sql
@@ -3,14 +3,14 @@ INSERT INTO tb_department(name) VALUES ('Personal Department');
 INSERT INTO tb_department(name) VALUES ('Financial');
 INSERT INTO tb_department(name) VALUES ('Administrative');
 INSERT INTO tb_department(name) VALUES ('Juridic');
-INSERT INTO tb_department(name) VALUES ('Business');
-INSERT INTO tb_department(name) VALUES ('Business Ineteligence');
-INSERT INTO tb_department(name) VALUES ('Informacion Technology');
+INSERT INTO tb_department(name) VALUES ('Business Intelligence');
+INSERT INTO tb_department(name) VALUES ('Information Technology');
 INSERT INTO tb_department(name) VALUES ('Sales');
 INSERT INTO tb_department(name) VALUES ('Client Success');
 INSERT INTO tb_department(name) VALUES ('Technical Support');
+INSERT INTO tb_department(name) VALUES ('NOC: Network Operations Center');
 
-INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (7, 'Relton Lima', 'dev.relton@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
