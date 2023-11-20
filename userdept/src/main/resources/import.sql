92c801d2d372046c07ca266184b55137603b5bf6
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-20 15:03:17 -0300

    update: add more departments to facilite testing

--- userdept/src/main/resources/import.sql
+++ userdept/src/main/resources/import.sql
@@ -1,5 +1,14 @@
-INSERT INTO tb_department(name) VALUES ('Gestão');
-INSERT INTO tb_department(name) VALUES ('Informática');
+INSERT INTO tb_department(name) VALUES ('Human Resources');
+INSERT INTO tb_department(name) VALUES ('Personal Department');
+INSERT INTO tb_department(name) VALUES ('Financial');
+INSERT INTO tb_department(name) VALUES ('Administrative');
+INSERT INTO tb_department(name) VALUES ('Juridic');
+INSERT INTO tb_department(name) VALUES ('Business');
+INSERT INTO tb_department(name) VALUES ('Business Ineteligence');
+INSERT INTO tb_department(name) VALUES ('Informacion Technology');
+INSERT INTO tb_department(name) VALUES ('Sales');
+INSERT INTO tb_department(name) VALUES ('Client Success');
+INSERT INTO tb_department(name) VALUES ('Technical Support');
 
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
