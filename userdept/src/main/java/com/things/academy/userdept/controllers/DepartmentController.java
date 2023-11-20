ed2419145f7ee63f4393f2892dc77cade5a304f3
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-20 00:44:20 -0300

    update: add get department by id and insert method from post request mapping

--- userdept/src/main/java/com/things/academy/userdept/controllers/DepartmentController.java
+++ userdept/src/main/java/com/things/academy/userdept/controllers/DepartmentController.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.things.academy.userdept.entities.Department;
+import com.things.academy.userdept.entities.User;
 import com.things.academy.userdept.repositories.DepartmentRepository;
 
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/departments")
 public class DepartmentController {
 	@Autowired
 	public DepartmentRepository repo;
@@ -21,6 +25,18 @@ public class DepartmentController {
 		List<Department> result = repo.findAll();
 		return result;
 		
+	}
+	
+	@GetMapping("/{id}")
+	public Department findById(@PathVariable Long id) {
+		Department result = repo.findById(id).get();
+		return result;
+	}
+	
+	@PostMapping
+	public Department insert(@RequestBody Department department) {
+		Department result = repo.save(department);
+		return department;
 	}
 
 }
