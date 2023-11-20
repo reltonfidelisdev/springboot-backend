e61d887c795e268702ad557fbd045366543cd508
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-19 23:57:23 -0300

    update: add new method to create an user record into database

--- userdept/src/main/java/com/things/academy/userdept/controllers/UserController.java
+++ userdept/src/main/java/com/things/academy/userdept/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,11 @@ public class UserController {
 		User result = repo.findById(id).get();
 		return result;
 		
+	}
+	
+	@PostMapping()
+	public User insert(@RequestBody User user) {
+		User result = repo.save(user); 
+		return user;
 	}
 }
