2d5f83dc22762ffeb90044f831c58487407b9531
Relton Fidelis <contato.reltonfidelis@gmail.com>
2023-11-19 23:03:50 -0300

    add persistence layer

--- userdept/src/main/java/com/things/academy/userdept/entities/User.java
+++ userdept/src/main/java/com/things/academy/userdept/entities/User.java
@@ -1,10 +1,24 @@
 package com.things.academy.userdept.entities;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "department_id")
 	private Department department;
 	
 	public User() {}
